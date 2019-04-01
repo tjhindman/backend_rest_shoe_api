94674b6a09fa02fb59f3c43f42a2df1eb153b47c
TJ Hindman <thind131@gmail.com>
2019-04-01 09:44:51 -0400

    bob comment added

--- backend_rest_shoe_api/quickstart/views.py
+++ backend_rest_shoe_api/quickstart/views.py
@@ -1,5 +1,8 @@
-"""Bob endured a difficult life in the African Savanna. For example, he could
-only afford to dress"""
+"""
+Bob endured a difficult life in the African Savanna. For example, he could
+only afford to dress in a single loin cloth and had to survive off of McDonald's
+Big Macs.
+"""
 
 
 from django.shortcuts import render
@@ -10,7 +13,7 @@ from backend_rest_shoe_api.quickstart.serializers import ManufacturerSerializer,
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows manufacturers to be viewed or edited.
     """
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializer
@@ -18,7 +21,7 @@ class ManufacturerViewSet(viewsets.ModelViewSet):
 
 class ShoeTypeViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows shoe types to be viewed or edited.
     """
     queryset = ShoeType.objects.all()
     serializer_class = ShoeTypeSerializer
@@ -26,7 +29,7 @@ class ShoeTypeViewSet(viewsets.ModelViewSet):
 
 class ShoeColorViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows shoe colors to be viewed or edited.
     """
     queryset = ShoeColor.objects.all()
     serializer_class = ShoeColorSerializer
@@ -34,7 +37,7 @@ class ShoeColorViewSet(viewsets.ModelViewSet):
 
 class ShoeViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows shoes to be viewed or edited.
     """
     queryset = Shoe.objects.all()
     serializer_class = ShoeSerializer
